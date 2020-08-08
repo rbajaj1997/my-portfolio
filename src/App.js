a0dc8d2ef9e022e38d104f1766d9854341be2312
Rohit Bajaj <rbajaj1402@gmail.com>
2020-08-08 16:56:04 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path="/about" component={About} />
+					<Route path="/projects" component={Projects} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/" component={Home} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+import logo from '../img/logo.png';
+
+export default function Navbar() {
+    return (
+        <nav class="navbar">
+            <div class="navbar__logo">
+                <NavLink to="/">
+                    <img className="logo" src={logo} alt="logo" />
+                </NavLink>
+            </div>
+            <ul class="navbar__menu">
+                <li>
+                    <NavLink
+                        to="/about"
+                        activeClassName="active-link"
+                        className="primary-link">
+                        About
+							</NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/projects"
+                        activeClassName="active-link"
+                        className="primary-link">
+                        Projects
+							</NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/contact"
+                        activeClassName="active-link"
+                        className="primary-link">
+                        Contact
+							</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
