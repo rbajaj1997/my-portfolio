4c3fda2007178dad7cb82c624df430af51a23c47
Rohit Bajaj <rbajaj1402@gmail.com>
2021-03-16 19:30:08 +0530

    Implementing lazy loading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import './App.scss';
+import React, { Suspense, lazy } from "react";
+import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+const About = lazy(() => import("./components/About"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
 
 function App() {
-	return (
-		<div className="container">
-			<Router basename={process.env.PUBLIC_URL}>
-				<Navbar />
-				<Switch>
-					<Route path="/about" exact component={About} />
-					<Route path="/projects" exact component={Projects} />
-					<Route path="/contact" exact component={Contact} />
-					<Route path="/" exact component={Home} />
-				</Switch>
-				<Footer />
-			</Router>
-		</div>
-	);
+  return (
+    <div className="container">
+      <Router basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Suspense fallback={<div className="loader"></div>}>
+          <Switch>
+            <Route path="/about" exact component={About} />
+            <Route path="/projects" exact component={Projects} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </Suspense>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
